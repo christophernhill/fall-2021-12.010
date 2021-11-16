37187120c8535ef816ce10c348be17e50e9e0f20
Chris Hill <cnh@mit.edu>
2021-11-16 10:51:56 -0500

    Add files via upload

---
+++ Julia/MyPoly/MyPoly/src/poly.jl
@@ -0,0 +1,68 @@
+"""
+  Define a poly type
+"""
+abstract type AbstractPolygon end
+
+struct MyPoly <: AbstractPolygon
+        plist::Vector{MyPoint}
+        nmax::Array{Int,1}
+        ncur::Array{Int,1}
+end
+
+# Join two polygon sets of points together
+# - creates a new poly
+function join(poly1::MyPoly, poly2::MyPoly)
+        ncur=length(poly1.plist) + length(poly2.plist)
+        MyPoly(vcat(poly1.plist,poly2.plist),ncur)
+end
+
+# Append a point to a polygon
+# - updates in place if there is free space, or otherwise 
+# - allocates and returns larger poly with orginal points
+# - copied over.
+function append!(poly::MyPoly, p::MyPoint)
+        ncur=poly.ncur[1]
+        nmax=poly.nmax[1]
+        if ncur+1 > nmax
+           nmax=nmax+100
+           poly=MyPoly(poly.plist,nmax)
+        end
+        poly.plist[ncur+1]=p
+        poly.ncur[1]=ncur+1
+        return poly
+end
+
+# Allocate a new poly from point list
+# - allocate at least nmax points 
+function MyPoly(p::Vector{MyPoint},nmax::Int)
+        # Set total size
+        ncur=length(p)
+        nmax=Int(ceil(ncur/nmax))*nmax
+        # Allocate and set default values
+        plist=Vector{MyPoint}(undef,nmax)
+        for i=1:nmax
+          plist[i]=MyPoint(-999, -999)
+        end
+        poly=MyPoly(plist,[nmax],[ncur])
+        # Set values for given points
+        poly.plist[1:ncur]=p
+        return poly
+end
+
+function area(poly::MyPoly)
+        ta=0.
+        p0=poly.plist[1]
+        for i=3:poly.ncur[1]
+                v1=poly.plist[i-1]-p0
+                v2=poly.plist[i  ]-p0
+                ta=ta+(v1.x*v2.y-v1.y*v2.x)*0.5
+        end
+        return ta
+end
+
+# Some inheritance
+struct MyRectangle <: AbstractPolygon
+        rpoly::MyPoly
+        width::Float64
+        height::Float64
+end
