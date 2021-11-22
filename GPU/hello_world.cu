341557af8e0d46790f9328a13b9902045490d670
Chris Hill <cnh@mit.edu>
2021-11-22 15:17:51 -0500

    Create hello_world.cu

---
+++ GPU/hello_world.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * A simple introduction to programming in CUDA. 
+ * This program prints "Hello World from GPU! from 10 CUDA threads running on the GPU."
+ */
+
+__global__ void helloFromGPU()
+{
+    printf("Hello World from GPU!\n");
+}
+
+int main(int argc, char **argv)
+{
+    printf("Hello World from CPU!\n");
+
+    helloFromGPU<<<1, 10>>>();
+    cudaDeviceReset();
+    return 0;
+}
